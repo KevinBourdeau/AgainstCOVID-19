7bf3e755b795fd69020aaf16756372cda77f1a7b
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2020-06-25 15:52:31 +0200

    Preparation merge
    Fix footer
    Ajout partie admin

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,8 +8,21 @@ use Twig\Environment;
 
 class MainController
 {
+
+  public function sessionStart() 
+  {
+    session_start();
+
+    if (isset($_SESSION['isAdmin']) == false)
+    {
+      $_SESSION['isAdmin'] = false;
+    }
+  }
+
   public function homePage(Environment $twig)
   {
+
+    $this->sessionStart();
 
     $file    = fopen( "../templates/Content/HomePage/section1.txt", "r" );
     $s1 = "";
@@ -34,7 +47,7 @@ class MainController
 
     $content = 
       $twig->render('Header/header.html.twig', ['page' => 'Accueil']) . 
-      $twig->render('Navbar/navbar.html.twig', ['isActive1' => 'active', 'isActive2' => null, 'isActive3' => null]) . 
+      $twig->render('Navbar/navbar.html.twig', ['isActive1' => 'active', 'isActive2' => null, 'isActive3' => null, 'isActive4' => null, 'isAdmin' => $_SESSION['isAdmin']]) . 
       $twig->render('Body/homePage.html.twig', ['s1' => $s1, 's2' => $s2, 's3' => $s3]) . 
       $twig->render('Footer/footer.html.twig') . 
       $twig->render('End/end.html.twig');
@@ -42,11 +55,14 @@ class MainController
     return new Response($content);
   }
 
+
   public function productPage(Environment $twig) {
+
+    $this->sessionStart();
 
     $content =
       $twig->render('Header/header.html.twig', ['page' => 'Produit']) . 
-      $twig->render('Navbar/navbar.html.twig', ['isActive1' => null, 'isActive2' => 'active', 'isActive3' => null]) . 
+      $twig->render('Navbar/navbar.html.twig', ['isActive1' => null, 'isActive2' => 'active', 'isActive3' => null, 'isActive4' => null, 'isAdmin' => $_SESSION['isAdmin']]) . 
       $twig->render('Body/productPage.html.twig') . 
       $twig->render('Footer/footer.html.twig') . 
       $twig->render('End/end.html.twig');
@@ -55,11 +71,44 @@ class MainController
 
   }
 
+
   public function demandePage(Environment $twig) {
+
+    $this->sessionStart();
 
     $content =
       $twig->render('Header/header.html.twig', ['page' => 'Demande']) . 
-      $twig->render('Navbar/navbar.html.twig', ['isActive1' => null, 'isActive2' => null, 'isActive3' => 'active']) .
+      $twig->render('Navbar/navbar.html.twig', ['isActive1' => null, 'isActive2' => null, 'isActive3' => 'active', 'isActive4' => null, 'isAdmin' => $_SESSION['isAdmin']]) .
+      $twig->render('Footer/footer.html.twig') . 
+      $twig->render('End/end.html.twig');
+
+    return new Response($content);
+
+  }
+
+  public function connectAdminPage(Environment $twig) {
+
+    $this->sessionStart();
+
+    $content =
+      $twig->render('Header/header.html.twig', ['page' => 'Demande']) . 
+      $twig->render('Navbar/navbar.html.twig', ['isActive1' => null, 'isActive2' => null, 'isActive3' => null, 'isActive4' => null, 'isAdmin' => $_SESSION['isAdmin']]) .
+      $twig->render('Body/connectAdmin.html.twig') . 
+      $twig->render('Footer/footer.html.twig') . 
+      $twig->render('End/end.html.twig');
+
+    return new Response($content);
+
+  }
+
+  
+  public function adminPage(Environment $twig) {
+
+    $this->sessionStart();
+
+    $content =
+      $twig->render('Header/header.html.twig', ['page' => 'Demande']) . 
+      $twig->render('Navbar/navbar.html.twig', ['isActive1' => null, 'isActive2' => null, 'isActive3' => null, 'isActive4' => 'active', 'isAdmin' => $_SESSION['isAdmin']]) .
       $twig->render('Footer/footer.html.twig') . 
       $twig->render('End/end.html.twig');
 
