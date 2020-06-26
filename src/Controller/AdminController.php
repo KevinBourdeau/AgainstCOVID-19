c71f3f2e3fadaa2d7450ebc8722c8df89b475a19
SlyverStorm <44494841+SlyverStorm@users.noreply.github.com>
2020-06-26 12:54:36 +0200

    Ajout Espace admin
    Table des demandes avec toutes les infos dans l'espace administrateur

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,6 +4,8 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
 use Twig\Environment;
 
 class AdminController
@@ -23,15 +25,37 @@ class AdminController
 
     $this->sessionStart();
 
+    $client = HttpClient::create();
+    $demandes = $client->request('GET', 'http://localhost:8080/api/demandes');
+    $demandes = json_decode($demandes->getContent());
+    
+
     $content =
       $twig->render('Header/header.html.twig', ['page' => 'Demande']) . 
       $twig->render('Navbar/navbar.html.twig', ['isActive1' => null, 'isActive2' => null, 'isActive3' => null, 'isActive4' => 'active', 'isAdmin' => $_SESSION['isAdmin']]) .
+      $twig->render('Body/tableTop.html.twig');
+
+    
+    foreach($demandes as $row)
+    {
+      $content .= $twig->render('Body/tableRow.html.twig', ['id' => $row->{'id'}, 'nom' => $row->{'nom'}, 'prenom' => $row->{'prenom'}, 'nom_etablissement' => $row->{'nom_etablissement'}, 'tel' => $row->{'tel'}, 'email' => $row->{'email'}, 'quantite' => $row->{'quantite'}, 'date' => $row->{'date'}, 'status' => $row->{'statut'}]); 
+    }
+    
+      
+    $content .=
+      $twig->render('Body/tableBottom.html.twig') .
       $twig->render('Footer/footer.html.twig') . 
       $twig->render('End/end.html.twig');
 
-    return new Response($content);
+    if ($_SESSION['isAdmin'])
+    {
+      return new Response($content);
+    }
+    else
+    {
+      return new Response('Access denied ! : error 403');
+    }
 
   }
 
-  
 }
